e2abca969a02e51b781f8ae08fde08c84e0ba0c3
Kaustubh0511 <kaustubhdayma@gmail.com>
2023-10-04 15:52:29 +0530

    Third Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,10 @@
 import logo from '../../src/logo.svg';
+import NavBar from './NavBar';
 
 const Header = () =>{
     return(
         <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
         </div>
     )   
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return(
+        <div class="bg-[#222] font-bold text-size:xl">
+            <p class=''>This is my react app</p>
+        </div>
+    )
+}
+
+export default NavBar;
