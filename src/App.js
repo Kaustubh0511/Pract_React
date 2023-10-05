102127ee93eb0a2353fc4cf72df05c42f5d6da2a
Kaustubh0511 <kaustubhdayma@gmail.com>
2023-10-05 10:09:03 +0530

    Learning Props Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Card/>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,10 @@
+const Card =() =>{
+    return(
+        <div class="bg-white rounded-lg shadow-lg p-4">
+            <h4 class="text-xl font-semibold mb-2">Hey There!!</h4>
+            <p class="text-gray-700">Let us learn Props</p>
+        </div>
+    )
+}
+
+export default Card;
